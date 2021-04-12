e2e2ffa8a6e02ddc0cf50a5ba75dbe887e558a95
SugarBlank <eneblank19@protonmail.com>
2021-04-11 22:50:26 -0400

    Update Arisa.py

--- ArisaBot/Arisa.py
+++ ArisaBot/Arisa.py
@@ -18,14 +18,21 @@ initial_extensions = [
     "cogs.Logger",
     "cogs.Info",
     "cogs.Moderation",
-    "cogs.Help"
+    "cogs.Help",
+    "cogs.Warn",
+    "cogs.Scrape",
+    "cogs.Chan",
+    
     ]
-
-
-
 
 for extension in initial_extensions:
     client.load_extension(extension)
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 @client.command(brief="Returns your ping.")
@@ -57,8 +64,6 @@ async def _8ball(ctx, *, question):
                  'My reply is no.']
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
-
-
 @client.command(brief="Bot spam pings, only accessible to admin, not suggested.")
 @commands.has_permissions(administrator=True)
 async def spam(ctx, spam="off"):
@@ -71,7 +76,7 @@ async def spam(ctx, spam="off"):
 @client.event
 async def on_ready():
     print(f"\nLogged in as {client.user} - {client.user.id}\nDiscord Version: {discord.__version__}\n")
-    print("Arisa logged in and booted.")
-
+    await client.change_presence(activity=discord.Game('Sanitizing the Lab (?help)'))
+    print("Weedy logged in and booted.")
 
 client.run("BOT TOKEN HERE")
