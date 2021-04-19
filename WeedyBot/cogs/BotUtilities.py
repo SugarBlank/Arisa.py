0395e0683647aa728c0aadf24e54880964ee0022
SugarBlank <eneblank19@protonmail.com>
2021-04-18 20:30:49 -0400

    Update BotUtilities.py

--- WeedyBot/cogs/BotUtilities.py
+++ WeedyBot/cogs/BotUtilities.py
@@ -17,13 +17,13 @@ class BotUtilities(commands.Cog):
 
     @Cog.listener()
     async def on_ready(self):
-        print("Bot Cog ready.")
+        print("BotUtilities Cog ready.")
 
     @commands.command(brief="Information about the Weedy bot.", aliases=["botinfo"], inline=True)
     async def bt(self, ctx):
         embed = discord.Embed(color=discord.Color(0xf8f8ff), description="**Weedy Bot Info:**")
         embed.set_thumbnail(url=ctx.bot.user.avatar_url)
-        embed.add_field(name="Developer:", value='scouri#7805')
+        embed.add_field(name="Developer:", value='scouri#1078')
         embed.add_field(name="Library:", value="discord.py")
         embed.add_field(name="Discord.py Version:", value=discord.__version__)
         embed.add_field(name="Test Server Invite:", value="https://discord.gg/FMxMHEQ")
