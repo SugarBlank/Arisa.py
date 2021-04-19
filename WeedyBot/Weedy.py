ee0ac07e5be29322fa04a20ea1db652f4712cea9
SugarBlank <eneblank19@protonmail.com>
2021-04-18 21:56:46 -0400

    Update Weedy.py

--- WeedyBot/Weedy.py
+++ WeedyBot/Weedy.py
@@ -1,43 +1,37 @@
 import random
 import discord
 from discord.ext import commands
-from glob import glob
-from pathlib import Path
-from discord.ext import menus
-import logging
+
 
 intents = discord.Intents.all()
 
 intents.members = True
 
-client = commands.Bot(command_prefix='?', case_insensitive=True, intents=intents)
+client = commands.Bot(command_prefix='?',
+                      case_insensitive=True,
+                      intents=intents)
 
 
 initial_extensions = [
-    "cogs.Bot",
-    "cogs.Logger",
-    "cogs.Info",
-    "cogs.Moderation",
-    "cogs.Help",
-    "cogs.Warn",
-    "cogs.Scrape",
+    "cogs.Anime",
+    "cogs.BotUtilities",
     "cogs.Chan",
-    
+    "cogs.ErrorHandler",
+    "cogs.Help",
+    "cogs.Info",
+    "cogs.Logger",
+    "cogs.Moderation",
+    "cogs.Scrape",
+    "cogs.Warn",
     ]
 
 for extension in initial_extensions:
     client.load_extension(extension)
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 
-
-@client.command(brief="Returns your ping.")
+@client.command(brief="Returns Weedy's ping.")
 async def ping(ctx):
-    await ctx.send(f":ping_pong: **{ctx.author}'s** latency is {round(client.latency * 1000)}ms!")
+    await ctx.send(f":ping_pong: **Weedy's** latency is {round(client.latency * 1000)}ms!")
 
 
 @client.command(aliases=['8ball', 'eightball'], brief="Obligatory 8ball command, for fun.")
@@ -64,14 +58,6 @@ async def _8ball(ctx, *, question):
                  'My reply is no.']
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
-@client.command(brief="Bot spam pings, only accessible to admin, not suggested.")
-@commands.has_permissions(administrator=True)
-async def spam(ctx, spam="off"):
-    while spam == "true":
-        await ctx.send("@everyone")
-        if spam == "off":
-            break
-
 
 @client.event
 async def on_ready():
@@ -79,4 +65,5 @@ async def on_ready():
     await client.change_presence(activity=discord.Game('Sanitizing the Lab (?help)'))
     print("Weedy logged in and booted.")
 
-client.run("BOT TOKEN HERE")
+
+client.run("BLANK")
